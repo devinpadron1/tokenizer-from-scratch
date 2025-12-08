6a2cd9188dc84dec45853d912728ec2d2debe364
Devin <devinpadron28@gmail.com>
2025-12-07 22:57:33 -0500

    a first pass at the byte pair algorithm

---
+++ byte_pair.py
@@ -0,0 +1,71 @@
+""" 
+Implement Byte Pair encoding
+1. Continuously iterate over your text
+2. Find the most frequent adjacent pairs of bytes
+    - how to store? dict with key being encoding as str, value being count?
+    - do we replace all in each pass or one per pass?
+    - could use a heap
+        - why?
+            - I wouldn't need to sort or iterate over
+3. Replace it with a new character
+    - which character?
+        - initially we'll have 
+4. Continue until all are unique(?)
+    - unsure about the stop condition (when no tokens with freq > 1?)
+
+Data Structures
+{} to count pairs    
+    - dict: pair (str) -> count (int)
+    - a max heap which contains highest count pair on top
+{} to store your vocabulary
+    - dict with mapping between abbreviated char and string
+    - ex Z -> aa
+"""
+
+
+from collections import defaultdict
+
+
+s = "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception."
+
+
+byte_list = list(s.encode("utf-8"))
+print(f"initial size of byte_list={len(byte_list)}")  # show byte pt num
+
+new_encodings = {}
+unused_code = 256
+
+# Byte pair loop
+while True:
+    # Count pairs
+    pairs = defaultdict(int) 
+    most_freq_pair = ["", 1]  
+   
+    # Gather most frequent pairs
+    for i in range(1, len(byte_list)):
+        pair = str(f"{byte_list[i]}+{byte_list[i - 1]}")
+        pairs[pair] += 1
+        freq = pairs[pair]
+        if freq > most_freq_pair[1]:
+            most_freq_pair = [pair, freq] 
+ 
+    print(f"most_freq_pair={most_freq_pair}")
+
+    if most_freq_pair[1] == 1:
+        break
+
+    new_encodings[unused_code] = most_freq_pair[0]
+    unused_code += 1
+
+    # Replace
+    for i in range(1, len(byte_list)):
+        pair = str(f"{byte_list[i]}+{byte_list[i - 1]}")
+        if pair == most_freq_pair[0]:
+            byte_list[i - 1] = unused_code
+            byte_list[i] = -1
+
+    # Remove -1s
+    byte_list = [byte for byte in byte_list if byte != -1]
+
+    print(f"size of byte_list {len(byte_list)}")
+
