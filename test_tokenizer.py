29cc3b33b2c25d64b3f115773b1ce501a418e645
Devin <devinpadron28@gmail.com>
2025-12-16 11:08:22 -0500

    add test file

---
+++ test_tokenizer.py
@@ -0,0 +1,34 @@
+from tokenizer import Tokenizer
+
+tests = [
+   "aaaabcdeaaaaghi", 
+   "aa",
+   "aaa",
+   "aaaa",
+   "aabaab",
+   "unfortunately the understanding was misunderstood",
+   "café naïve résumé",
+   "x",
+   "",
+   "the theater thermal theme"
+]
+
+for test in tests:
+    print()
+    print(f"  input data => {test}")
+    # print(list(data.encode("utf-8")))
+
+    t = Tokenizer(training_data=test)
+    t.train()
+
+    encoded_data = t.encode(test)
+    print(f"encoded data => {encoded_data}")
+    # print([ord(d) for d in encoded_data])
+
+    decoded_data = t.decode(encoded_data)
+    print(f"decoded data => {decoded_data}")
+
+    print()
+    print("success! :-)") if test == decoded_data else print("tokenizer failed :-(")
+    print()
+
