b4428ee344d74755c90978214924dd02bee2d830
Devin <devinpadron28@gmail.com>
2025-12-31 11:15:17 -0400

    rm prints

--- test_tokenizer.py
+++ test_tokenizer.py
@@ -19,12 +19,8 @@ for test in tests:
 
     t = Tokenizer()
     t.train(training_data=test)
-
     encoded_data = t.encode(test)
-    print(f"encoded data => {encoded_data}")
-
     decoded_data = t.decode(encoded_data)
-    print(f"decoded data => {decoded_data}")
 
     print()
     print("success! :-)") if test == decoded_data else print("tokenizer failed :-(")
